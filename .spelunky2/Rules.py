7ec5d619cdfe9f2dbd23a9926f42790b717a44c5
Ontikarus Ryan <realddrkhat@gmail.com>
2025-08-22 21:28:01 +0100

    Correct AP rules to account for not being locked

--- .spelunky2/Rules.py
+++ .spelunky2/Rules.py
@@ -7,6 +7,12 @@ from worlds.generic.Rules import set_rule
 if TYPE_CHECKING:
     from . import Spelunky2World
 
+def has_or_unrestricted(world, state, player, item_name: str) -> bool:
+    """
+    Returns True if the item is in the player's possession OR
+    is not in the AP-locked 'restricted_items' set (meaning it's free in-game).
+    """
+    return (item_name not in world.options.restricted_items.value) or state.has(item_name, player)
 
 def set_common_rules(world: "Spelunky2World", player: int):
 
@@ -26,50 +32,52 @@ def set_common_rules(world: "Spelunky2World", player: int):
 
     # Secondary Regions
     set_rule(world.get_entrance("Dwelling -> Any World 2"), lambda state: has_world_2(state, player))
-    set_rule(world.get_entrance("Jungle -> Black Market"), lambda state: state.has("Udjat Eye", player))  # TODO Udjat Skip setting
-    set_rule(world.get_entrance("Volcana -> Vlad's Castle"), lambda state: state.has("Udjat Eye", player))  # TODO Udjat Skip setting
-    set_rule(world.get_entrance("Tide Pool -> Abzu"), lambda state: state.has("Ankh", player))  # TODO Deathskip setting
-    set_rule(world.get_entrance("Temple -> City of Gold"), lambda state: has_royalty(state, player) and state.has("Scepter", player))
-    set_rule(world.get_entrance("City of Gold -> Duat"), lambda state: state.has("Ankh", player))
+    set_rule(world.get_entrance("Jungle -> Black Market"), lambda state: has_or_unrestricted(world, state, player, "Udjat Eye"))  # TODO Udjat Skip setting
+    set_rule(world.get_entrance("Volcana -> Vlad's Castle"), lambda state: has_or_unrestricted(world, state, player, "Udjat Eye"))  # TODO Udjat Skip setting
+    set_rule(world.get_entrance("Tide Pool -> Abzu"), lambda state: has_or_unrestricted(world, state, player, "Ankh"))  # TODO Deathskip setting
+    set_rule(world.get_entrance("Temple -> City of Gold"), lambda state: has_royalty(world, state, player) and has_or_unrestricted(world, state, player, "Scepter"))
+    set_rule(world.get_entrance("City of Gold -> Duat"), lambda state: has_or_unrestricted(world, state, player, "Ankh"))
     set_rule(world.get_entrance("Ice Caves -> Mothership"), lambda state: can_access_mothership(state, player))
 
     # People Entries
-    set_rule(world.get_location("Eggplant Child Journal Entry"), lambda state: state.has("Eggplant", player))
+    set_rule(world.get_location("Eggplant Child Journal Entry"), lambda state: has_or_unrestricted(world, state, player, "Eggplant"))
 
     # Bestiary Entries
-    set_rule(world.get_location("Qilin Journal Entry"), lambda state: can_obtain_qilin(state, player))
+    set_rule(world.get_location("Qilin Journal Entry"), lambda state: can_obtain_qilin(world, state, player))
 
     # Item Entries
     set_rule(world.get_location("Alien Compass Journal Entry"), lambda state: can_obtain_alien_compass(state, player))
-    set_rule(world.get_location("Udjat Eye Journal Entry"), lambda state: state.has("Udjat Eye", player))
-    set_rule(world.get_location("Hedjet Journal Entry"), lambda state: state.has("Hedjet", player))
-    set_rule(world.get_location("Crown Journal Entry"), lambda state: state.has("Crown", player))
-    set_rule(world.get_location("Ankh Journal Entry"), lambda state: state.has("Ankh", player))
-    set_rule(world.get_location("Tablet of Destiny Journal Entry"), lambda state: can_obtain_tablet(state, player))
-    set_rule(world.get_location("Excalibur Journal Entry"), lambda state: has_royalty(state, player) and state.has("Excalibur", player))
-    set_rule(world.get_location("Scepter Journal Entry"), lambda state: state.has("Scepter", player))
-    set_rule(world.get_location("Hou Yi's Bow Journal Entry"), lambda state: state.has("Hou Yi's Bow", player))
-    set_rule(world.get_location("Ushabti Journal Entry"), lambda state: state.has("Ushabti", player))
-    set_rule(world.get_location("Eggplant Journal Entry"), lambda state: state.has("Eggplant", player))
+    set_rule(world.get_location("Udjat Eye Journal Entry"), lambda state: has_or_unrestricted(world, state, player, "Udjat Eye"))
+    set_rule(world.get_location("Hedjet Journal Entry"), lambda state: has_or_unrestricted(world, state, player, "Hedjet"))
+    set_rule(world.get_location("Crown Journal Entry"), lambda state: has_or_unrestricted(world, state, player, "Crown"))
+    set_rule(world.get_location("Ankh Journal Entry"), lambda state: has_or_unrestricted(world, state, player, "Ankh"))
+    set_rule(world.get_location("Tablet of Destiny Journal Entry"), lambda state: can_obtain_tablet(world, state, player))
+    set_rule(world.get_location("Excalibur Journal Entry"), lambda state: has_royalty(world, state, player) and has_or_unrestricted(world, state, player, "Excalibur"))
+    set_rule(world.get_location("Scepter Journal Entry"), lambda state: has_or_unrestricted(world, state, player, "Scepter"))
+    set_rule(world.get_location("Hou Yi's Bow Journal Entry"), lambda state: has_or_unrestricted(world, state, player, "Hou Yi's Bow"))
+    set_rule(world.get_location("Ushabti Journal Entry"), lambda state: has_or_unrestricted(world, state, player, "Ushabti"))
+    set_rule(world.get_location("Eggplant Journal Entry"), lambda state: has_or_unrestricted(world, state, player, "Eggplant"))
 
 
 def set_sunken_city_rules(world: "Spelunky2World", player: int):
 
     # Entrance Rules
-    set_rule(world.get_entrance("Neo Babylon -> Sunken City"), lambda state: can_access_sunken_city(state, player))
-    set_rule(world.get_entrance("Sunken City -> Eggplant World"), lambda state: state.has("Eggplant", player))
-    set_rule(world.get_location("Arrow of Light Journal Entry"), lambda state: can_access_sunken_city(state, player))
+    set_rule(world.get_entrance("Neo Babylon -> Sunken City"), lambda state: can_access_sunken_city(world, state, player))
+    set_rule(world.get_entrance("Sunken City -> Eggplant World"), lambda state: has_or_unrestricted(world, state, player, "Eggplant"))
+    set_rule(world.get_location("Arrow of Light Journal Entry"), lambda state: can_access_sunken_city(world, state, player))
 
 
 def set_cosmic_ocean_rules(world: "Spelunky2World", player: int):
-    set_rule(world.get_entrance("Sunken City -> Cosmic Ocean"), lambda state: can_access_cosmic_ocean(state, player))
+    set_rule(world.get_entrance("Sunken City -> Cosmic Ocean"), lambda state: can_access_cosmic_ocean(world, state, player))
 
 
-def has_royalty(state: CollectionState, player: int):
-    return (state.has("Udjat Eye", player)
-            and (state.has_all(["Jungle", "Hedjet"], player)
-                 or state.has_all(["Volcana", "Crown"], player)
-                 or (state.has("Progressive World Unlock", player) and state.has_any(["Hedjet", "Crown"], player))))
+def has_royalty(world: "Spelunky2World", state: CollectionState, player: int):
+    return (has_or_unrestricted(world, state, player, "Udjat Eye")
+            and (state.has_all(["Jungle"], player) and has_or_unrestricted(world, state, player, "Hedjet")
+                 or state.has_all(["Volcana"], player) and has_or_unrestricted(world, state, player, "Crown")
+                 or (state.has("Progressive World Unlock", player)
+                     and (has_or_unrestricted(world, state, player, "Hedjet")
+                          or has_or_unrestricted(world, state, player, "Crown")))))
 
 
 def has_weapon(state: CollectionState, player: int) -> bool:  # Currently unused
@@ -95,22 +103,24 @@ def can_access_mothership(state: CollectionState, player: int) -> bool:
     return can_obtain_alien_compass(state, player)  # or state.has("Progressive Compass", player, 2) -- not valid, as Alien Compass is only given at start if you already have the journal entry
 
 # TODO Excalibur Skip settings
-def can_obtain_tablet(state: CollectionState, player: int) -> bool:
-    return (state.has("Tablet of Destiny", player) and
-            (state.can_reach("Duat", "Region", player) or
-             (state.can_reach("Abzu", "Region", player) and state.can_reach("Excalibur Journal Entry", "Location", player))))
+def can_obtain_tablet(world: "Spelunky2World", state: CollectionState, player: int) -> bool:
+    return (has_or_unrestricted(world, state, player, "Tablet of Destiny")
+            and (state.can_reach("Duat", "Region", player)
+                 or (state.can_reach("Abzu", "Region", player)
+                     and state.can_reach("Excalibur Journal Entry", "Location", player))))
 
-
-def can_obtain_qilin(state: CollectionState, player: int) -> bool:
-    return can_obtain_tablet(state, player) and state.has("Ushabti", player)
+def can_obtain_qilin(world: "Spelunky2World", state: CollectionState, player: int) -> bool:
+    return (can_obtain_tablet(world, state, player)
+            and has_or_unrestricted(world, state, player, "Ushabti"))
 
 # TODO Qilin Skip settings
-def can_access_sunken_city(state: CollectionState, player: int) -> bool:
-    return can_obtain_qilin(state, player) and (state.has("Sunken City", player) or state.has("Progressive World Unlock", player, 6))
+def can_access_sunken_city(world: "Spelunky2World", state: CollectionState, player: int) -> bool:
+    return can_obtain_qilin(world, state, player) and (state.has("Sunken City", player) or state.has("Progressive World Unlock", player, 6))
 
 
-def can_access_cosmic_ocean(state: CollectionState, player: int) -> bool:
-    return (state.has_all(["Hou Yi's Bow", "Arrow of Light"], player)
+def can_access_cosmic_ocean(world: "Spelunky2World", state: CollectionState, player: int) -> bool:
+    return (has_or_unrestricted(world, state, player, "Hou Yi's Bow")
+            and state.has("Arrow of Light", player)
             and (state.has("Cosmic Ocean", player) or state.has("Progressive World Unlock", player, 7)))
 
 
@@ -134,7 +144,7 @@ def set_starter_upgrade_rules(world: "Spelunky2World", player: int):
             try:
                 loc = world.get_location(waddler_name)
                 # Waddler items require their locked item counterpart
-                set_rule(loc, lambda state, name=item_name: state.has(name, player))
+                set_rule(loc, lambda state, name=item_name: has_or_unrestricted(world, state, player, name))
             except KeyError:
                 pass
 
@@ -146,6 +156,6 @@ def set_starter_upgrade_rules(world: "Spelunky2World", player: int):
             try:
                 loc = world.get_location(upgrade_name)
                 # Upgrades require their locked item counterpart
-                set_rule(loc, lambda state, name=item_name: state.has(name, player))
+                set_rule(loc, lambda state, name=item_name: has_or_unrestricted(world, state, player, name))
             except KeyError:
                 pass
